ed2e1fd10b5b16ffc0f7103594e238cafcef16a7
Muhammad <muhammadolaniyan56@gmail.com>
2025-02-18 13:29:27 -0800

    Game version1

--- maze_game/maze_game/settings.py
+++ maze_game/maze_game/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from os import path
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,6 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
